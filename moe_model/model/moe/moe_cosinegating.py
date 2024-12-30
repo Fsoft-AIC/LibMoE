c10dab7d375b848a8521abde35430c660121b36f
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-12-30 23:07:32 +0700

    v1.1

---
+++ moe_model/model/moe/moe_cosinegating.py
@@ -0,0 +1,51 @@
+
+import torch
+import torch.nn as nn
+from einops import rearrange, repeat, reduce, pack, unpack
+import torch.nn.functional as F
+
+from .register import register_moe
+from .moe import MoeLayer
+
+
+@register_moe("smoe_cosinegating")
+class MoECosingGating(MoeLayer):
+    def __init__(self, in_embed_dim=768, out_embed_dim=768, num_of_experts=4, num_selected=2, expert=None, args=None):
+        super().__init__(in_embed_dim, out_embed_dim, num_of_experts, num_selected, expert, args)
+
+        self.gate = nn.Linear(in_embed_dim, num_of_experts, bias=True)
+
+
+    def _keepTopk(self, x, beta):
+        '''
+        KeepTopK(v, u):
+
+            --> v + u if v in the top K elements of vpp
+            --> -inf otherwise
+            
+        In this code, x = v and u = beta
+        '''
+        x = x + beta
+        weights, selected_experts  = torch.topk(x, k = self.num_selected, dim=2) 
+        weights = torch.softmax(weights, dim=2)
+        return weights, selected_experts 
+
+    
+    def forward(self, x, return_id_experts = False, is_vision = False):
+        # compute output
+
+        gate_logits = (x @ self.gate.weight.T) / (torch.norm(x, dim=-1, keepdim=True) * torch.norm(self.gate.weight, dim=1, keepdim=True).T)
+        
+        gate_softmax = F.softmax(gate_logits, dim=-1, dtype=torch.float).to(x.dtype)
+        weights, selected_experts = self._keepTopk(gate_softmax, self.gate.bias)
+        output = torch.zeros(x.shape[0], x.shape[1], self.out_embed_dim, device=x.device, dtype=x.dtype)
+        output = self.compute_moe(selected_experts, weights, output, x)
+        
+        auxiliary_loss = torch.tensor(0.0, device=x.device, dtype=x.dtype)
+        balance_loss = torch.tensor(0.0, device=x.device, dtype=x.dtype)
+        auxiliary_loss = self.combine_loss(selected_experts, gate_softmax, gate_logits)
+        
+        if return_id_experts:
+            return output, auxiliary_loss, selected_experts, balance_loss
+        else:
+            return output, auxiliary_loss, None, balance_loss
---
+++ scripts/eval/run_eval.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+export API_TYPE=''                          # OpenAI API type
+export DEPLOYMENT=''                        # OpenAI API deployment
+export ENDPOINT=''                          # OpenAI API endpoint
+export VERSION=''                           # OpenAI API version
+export API_KEY=''                           # OpenAI API key
+export KEY_HF=""                            # Hugging Face API key
+export CUDA_LAUNCH_BLOCKING=0
+export HF_HOME="../evaluate/lmms-eval"         # Path to lmms-eval
+export TMPDIR=""                            # Path to tmp directory
+export TOOLKIT_DIR=""                       # Path to the toolkitmoe directory
+export PYTHONPATH="../LibMoE":$PYTHONPATH
+
+
+# GPU setup
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+num_gpus=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
+
+# List of models to evaluate
+models=(
+    # conv_template|pretrained_path
+    "phi3|/checkpoints/phi3mini-siglip224/sft/smoe"  
+)
+
+# Evaluation
+cd ./evaluate
+for model in "${models[@]}"
+do
+    part=$(echo $model | cut -d'|' -f1)
+    PRETRAINED_PATH=$(echo $model | cut -d'|' -f2)
+    
+    # Task list
+    echo "Running evaluation for model: $PRETRAINED_PATH with conv_template: $part"
+    tasks=(
+        "ai2d"
+        "textvqa"
+        "gqa" 
+        "mmbench_en" 
+        "mme" 
+        "mmmu_val" 
+        "mmstar" 
+        "pope" 
+        "scienceqa" 
+        "mathvista_testmini" 
+        "hallusion_bench_image"
+    )
+
+    # Run evaluation
+    for task in "${tasks[@]}"
+    do
+        task_name=$(echo $task | tr ',' '_')
+        python3 -m accelerate.commands.launch \
+            --main_process_port=29511 \
+            --num_processes=$num_gpus \
+            -m lmms_eval \
+            --model llava \
+            --model_args pretrained="$PRETRAINED_PATH",conv_template="$part" \
+            --tasks $task \
+            --batch_size 1 \
+            --log_samples \
+            --log_samples_suffix llava_v1.5_${task_name} \
+            --output_path ./logs/ 
+    done
+done
