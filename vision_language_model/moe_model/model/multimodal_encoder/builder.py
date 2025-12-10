52fa34981f008b26aee89eff1be608b676b63b21
Nguyễn Văn Nam <97231719+DAVID-NGUYEN-S16@users.noreply.github.com>
2025-12-10 23:41:03 +0700

    Refactor vision tower path checks in builder.py

--- vision_language_model/moe_model/model/multimodal_encoder/builder.py
+++ vision_language_model/moe_model/model/multimodal_encoder/builder.py
@@ -5,13 +5,12 @@ from .siglip_encoder import SiglipVisionTower
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
-    is_absolute_path_exists = os.path.exists(vision_tower)
-    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion"):
+    if vision_tower.startswith("openai") or vision_tower.startswith("laion"):
         print("=========================================")
         print("=============CLIP-vision_tower=========")
         print("=========================================")
         return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-    elif is_absolute_path_exists or vision_tower.startswith("google"):
+    elif vision_tower.startswith("google"):
         print("=========================================")
         print("=============SigLIP-vision_tower=========")
         print("=========================================")
